9b458dc3fc3c0408b0260781e1c0838ca22d570b
bluerface <fallingsilkworm@gmail.com>
2017-02-12 14:19:41 +0000

    [FEAT-1] Add React/webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const validate = require('webpack-validator');
+
+const config = {
+  entry: __dirname + '/client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/public'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        loaders: ['style', 'css', 'sass']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devtool: 'eval-source-map'
+};
+
+module.exports = validate(config);
