0d8122ebd5c9fac6766a32017c15141d221f6f9d
bluerface <fallingsilkworm@gmail.com>
2017-02-12 16:51:52 +0000

    [FEAT-2] Create dummy data

---
+++ client/data/index.js
@@ -0,0 +1,140 @@
+module.exports = [
+  {
+    title: 'Northcoders',
+    description: 'Projects completed while working on the Northcoders course',
+    projects: [
+      {
+        imageSmall: '#',
+        imageBanner: '#',
+        title: 'Snaks',
+        slug: 'snaks',
+        description: 'A fun little game I made in JavaScript, where you must survive in a dessert full of Snaks.',
+        longDescription: 'I am some markdown lol'
+      },
+      {
+        imageSmall: '#',
+        imageBanner: '#',
+        title: 'Snaks',
+        slug: 'snaks',
+        description: 'A fun little game I made in JavaScript, where you must survive in a dessert full of Snaks.',
+        longDescription: 'I am some markdown lol'
+      },
+      {
+        imageSmall: '#',
+        imageBanner: '#',
+        title: 'Snaks',
+        slug: 'snaks',
+        description: 'A fun little game I made in JavaScript, where you must survive in a dessert full of Snaks.',
+        longDescription: 'I am some markdown lol'
+      },
+      {
+        imageSmall: '#',
+        imageBanner: '#',
+        title: 'Snaks',
+        slug: 'snaks',
+        description: 'A fun little game I made in JavaScript, where you must survive in a dessert full of Snaks.',
+        longDescription: 'I am some markdown lol'
+      },
+      {
+        imageSmall: '#',
+        imageBanner: '#',
+        title: 'Snaks',
+        slug: 'snaks',
+        description: 'A fun little game I made in JavaScript, where you must survive in a dessert full of Snaks.',
+        longDescription: 'I am some markdown lol'
+      }
+    ]
+  },
+  {
+    title: 'Northcoders',
+    description: 'Projects completed while working on the Northcoders course',
+    projects: [
+      {
+        imageSmall: '#',
+        imageBanner: '#',
+        title: 'Snaks',
+        slug: 'snaks',
+        description: 'A fun little game I made in JavaScript, where you must survive in a dessert full of Snaks.',
+        longDescription: 'I am some markdown lol'
+      },
+      {
+        imageSmall: '#',
+        imageBanner: '#',
+        title: 'Snaks',
+        slug: 'snaks',
+        description: 'A fun little game I made in JavaScript, where you must survive in a dessert full of Snaks.',
+        longDescription: 'I am some markdown lol'
+      },
+      {
+        imageSmall: '#',
+        imageBanner: '#',
+        title: 'Snaks',
+        slug: 'snaks',
+        description: 'A fun little game I made in JavaScript, where you must survive in a dessert full of Snaks.',
+        longDescription: 'I am some markdown lol'
+      },
+      {
+        imageSmall: '#',
+        imageBanner: '#',
+        title: 'Snaks',
+        slug: 'snaks',
+        description: 'A fun little game I made in JavaScript, where you must survive in a dessert full of Snaks.',
+        longDescription: 'I am some markdown lol'
+      },
+      {
+        imageSmall: '#',
+        imageBanner: '#',
+        title: 'Snaks',
+        slug: 'snaks',
+        description: 'A fun little game I made in JavaScript, where you must survive in a dessert full of Snaks.',
+        longDescription: 'I am some markdown lol'
+      }
+    ]
+  },
+  {
+    title: 'Northcoders',
+    description: 'Projects completed while working on the Northcoders course',
+    projects: [
+      {
+        imageSmall: '#',
+        imageBanner: '#',
+        title: 'Snaks',
+        slug: 'snaks',
+        description: 'A fun little game I made in JavaScript, where you must survive in a dessert full of Snaks.',
+        longDescription: 'I am some markdown lol'
+      },
+      {
+        imageSmall: '#',
+        imageBanner: '#',
+        title: 'Snaks',
+        slug: 'snaks',
+        description: 'A fun little game I made in JavaScript, where you must survive in a dessert full of Snaks.',
+        longDescription: 'I am some markdown lol'
+      },
+      {
+        imageSmall: '#',
+        imageBanner: '#',
+        title: 'Snaks',
+        slug: 'snaks',
+        description: 'A fun little game I made in JavaScript, where you must survive in a dessert full of Snaks.',
+        longDescription: 'I am some markdown lol'
+      },
+      {
+        imageSmall: '#',
+        imageBanner: '#',
+        title: 'Snaks',
+        slug: 'snaks',
+        description: 'A fun little game I made in JavaScript, where you must survive in a dessert full of Snaks.',
+        longDescription: 'I am some markdown lol'
+      },
+      {
+        imageSmall: '#',
+        imageBanner: '#',
+        title: 'Snaks',
+        slug: 'snaks',
+        description: 'A fun little game I made in JavaScript, where you must survive in a dessert full of Snaks.',
+        longDescription: 'I am some markdown lol'
+      }
+    ]
+  }
+];
