b5d645a4bbca233a40228c9acd8886a2168fe66f
bluerface <fallingsilkworm@gmail.com>
2017-02-15 10:20:21 +0000

    [FEAT-6] Add link styles

--- client/components/Header.js
+++ client/components/Header.js
@@ -3,7 +3,7 @@ import {Link} from 'react-router';
 import '../styles/Header.scss';
 
 const Header = () => (
-  <Link to='/' className='header-link'>
+  <Link to='/' className='block-link'>
     <header>
       <h1>{'Zoe\'s Portfolio'}</h1>
       <p>Welcome to Zoes portfolio.</p>
