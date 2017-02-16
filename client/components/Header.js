18889871703f16fc51ae481d37a711d67ca649de
bluerface <fallingsilkworm@gmail.com>
2017-02-16 16:08:56 +0000

    [FEAT-7] Edit header text

--- client/components/Header.js
+++ client/components/Header.js
@@ -6,8 +6,12 @@ const Header = () => (
   <Link to='/' className='block-link'>
     <header>
       <h1>{'Zoe\'s Portfolio'}</h1>
-      <p>Welcome to Zoes portfolio.</p>
-      <p>This site is under development, appologies for the bare bones look.</p>
+      <p className='blurb'>
+        A Junior Web Developer with strong skills in JavaScript and experience
+       with front and back end technologies. Currently pursuing a job in a
+       varied and creative environment, with an interest in design, UX and
+       technology as a force for social good.
+      </p>
     </header>
   </Link>
 );
